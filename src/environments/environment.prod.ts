a54e29f634e9b4844493f88ba135bbb15a1614f8
Administrator <Administrator@MORPH1>
2023-10-18 19:11:55 +0100

    src folder

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl : 'http://10.244.5.19:85/',
+  encryptKey: "253D3FB468A0E24677C28A624BE0F939",
+  iv : "00000000000000000000000000000000",
+  defaultPwd : 'pa55W0rd'
+};
---
+++ src/environments/environment.win.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl : 'http://172.107.203.118:80/',
+  encryptKey: "253D3FB468A0E24677C28A624BE0F939",
+  iv : "00000000000000000000000000000000",
+  defaultPwd : 'pa55W0rd'
+};
