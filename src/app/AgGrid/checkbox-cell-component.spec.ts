610fec4bada95e0c8affd92c41cf74ec2439c876
Priteseh <pritesh3@hotmail.co.uk>
2023-12-03 00:24:46 -0500

    removed spec file

--- src/app/AgGrid/checkbox-cell-component.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CheckBoxCellComponent } from './checkbox-cell-component';
-
-describe('CheckboxCellComponent', () => {
-  it('should create an instance', () => {
-    expect(new CheckBoxCellComponent(null)).toBeTruthy();
-  });
-});
